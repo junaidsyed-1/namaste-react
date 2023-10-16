ae440721f1d852d0533ea8955d194c59d0de9815
junaid <jaanusd79@gmail.com>
2023-10-16 21:16:09 +0530

    Second commit

---
+++ hello_world_react/App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World Using React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
