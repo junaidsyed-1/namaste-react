1bb0d42b09bed78b3306238180946537e6e001af
junaid <jaanusd79@gmail.com>
2023-10-21 14:40:52 +0530

    Functional Component

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Revision
 
-// React.createElement => React Element => Object => HTML
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React🚀");
+// React Element 
+const heading = (
+    <h1 className="heading" tabIndex="1">
+        React Element
+    </h1>
+)
 
-// JSX
-// JSX => React.createElement => React Element => Object => HTML
-// JSX is different, its not HTML inside JS, JSX is different and React is different. Babel is transpiling the code before it reaches to JS Engine.
-const jsxHeading = <h1>This is Namaste React using JSX</h1>
+// React Functional Component
+const HeadingComponent = () => (<h1>This is a React Functional Component</h1>);
 
-// ReactDOM converts the react element which is an object to HTML
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
