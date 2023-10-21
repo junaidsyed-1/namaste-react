f343f9e183e24be6a6f12c0cc2268e4237f99fac
junaid <jaanusd79@gmail.com>
2023-10-22 05:23:33 +0530

    React ELement inside Component

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-// React Element 
-const heading = (
+// React Element
+// How can i put React Element inside a component?
+// React Element is what? - It is a object and at the end of the day it is just JavaScript
+const title = (
     <h1 className="heading" tabIndex="1">
-        React Element
+        Namaste React🚀
     </h1>
-)
+);
 
 // React Functional Component
-const HeadingComponent = () => (<h1>This is a React Functional Component</h1>);
+const HeadingComponent = () => (
+    <div className="container">
+        {title}
+        <h1>This is a React Functional Component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
