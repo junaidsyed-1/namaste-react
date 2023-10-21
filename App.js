d981cf65c3c670bef02c13a4cad596ed716cb57a
junaid <jaanusd79@gmail.com>
2023-10-21 06:45:57 +0530

    Revision

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement(
-    "div", { id: "parent" }, [
-    React.createElement("div", { id: "child" },
-        [
-            React.createElement("h1", {}, "I am a h1 Tag"),
-            React.createElement("h2", {}, "I am a h2 Tag")
-        ]),
-    React.createElement("div", { id: "child2" },
-        [
-            React.createElement("h1", {}, "I am a h1 Tag"),
-            React.createElement("h2", {}, "I am a h2 Tag")
-        ]), 
-])
+// Revision
 
-console.log(parent)
+// React.createElement => Object
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React🚀");
 
+// ReactDOM converts the react element which is an object to HTML
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
