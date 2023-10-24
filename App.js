2c8c940fca904a1c520062225345f9d02cf5418c
junaid <jaanusd79@gmail.com>
2023-10-24 06:40:33 +0530

    Food Order App

--- App.js
+++ App.js
@@ -1,24 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-    <h1 className="title" tabIndex="1">
-        Namaste React🚀
-    </h1>
-);
+/**
+ * Components we need to build our Food Ordering App
+ * 
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ *      - Card Image
+ *      - Info
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Contact
+ *  - Address
+ * 
+ */
 
-// React Functional Component
-// Component Composition - It means that we can call a component inside a component.
-// Also we can call a component using 3 methods.
-const HeadingComponent = () => (
-    <div className="container">
-        {Title()} {/* I can also call a component like this because its just a normal JS function */}
-        <Title />
-        <Title></Title>
-        <h1 className="heading">This is a React Functional Component</h1>
-    </div>
-);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://cdn.dribbble.com/users/5462907/screenshots/11960844/5.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-card-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b" />
+            <h3>SubWay</h3>
+            <h4>Salads, Snacks, Desserts, Beverages</h4>
+            <h4>4.1 stars</h4>
+            <h4>31 MINS</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search Bar</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
