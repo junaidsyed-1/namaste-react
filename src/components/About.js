0c76aa5d57baf81301886fe6f9e658befbd6296c
junaid <jaanusd79@gmail.com>
2023-11-03 06:51:33 +0530

    Basic Class Based Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
+import User from './User'
+import UserClass from './UserClass'
+
 const About = () => {
     return (
         <div id="about">
             <h1>This is About component</h1>
+            <UserClass name={"Junaid Syed class"} location={"Delhi (class)"} />
         </div>
     );
 };
