0c58eeac86f1ef31b5bbe9dee3d0b1e71d794f52
junaid <jaanusd79@gmail.com>
2023-11-21 04:30:47 +0530

    updated

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -13,3 +13,22 @@ test("Should load the contact us component", () => {
     expect(heading).toBeInTheDocument();
 
 });
+
+test("Should load the button inside contact component", () => {
+
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+
+});
+
+test("Should load the input placeHolder name inside contact component", () => {
+
+    render(<Contact />);
+
+    const inputName = screen.getByPlaceholderText('name');
+
+    expect(inputName).toBeInTheDocument();
+})
