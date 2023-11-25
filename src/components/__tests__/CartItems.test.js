116580f0ae1df529dc500da237eb1c19040e8da6
junaid <jaanusd79@gmail.com>
2023-11-25 12:45:04 +0530

    Formated the CartItems Integration

--- src/components/__tests__/CartItems.test.js
+++ src/components/__tests__/CartItems.test.js
@@ -6,35 +6,77 @@ import { Provider } from 'react-redux';
 import appStore from '../../utils/appStore';
 import "@testing-library/jest-dom"
 import { BrowserRouter } from 'react-router-dom';
-import Header from '../Header'
+import Header from '../Header';
+import Cart from '../Cart'
 
 
 global.fetch = jest.fn(() => Promise.resolve({
     json: () => Promise.resolve(MOCK_DATA)
 }));
 
-it("should load the menu component", async () => {
-
-    await act(async () => render(
-        <BrowserRouter>
-            <Provider store={appStore}>
-                <Header />
-                <RestaurantMenu />
-            </Provider>
-        </BrowserRouter>
-    ));
-
-    const accordionHeader = screen.getByText("Chinese Economic Combo [2 Boxes] (6)");
-    fireEvent.click(accordionHeader);
-
-    const addBtns = screen.getAllByRole('button', { name: "Add +" })
-    fireEvent.click(addBtns[0]);
-
-    // Check if the item added to the cart
-    expect(screen.getByText('Cart - (1 Items)')).toBeInTheDocument();
-
-    expect(screen.getAllByTestId('foodItems').length).toBe(6);
 
 
+describe("It should load the Menu Items and it should add the items to the cart, also display the cart component", () => {
+
+    it("should load the menu component with Accordion Header", async () => {
+
+        await act(async () => render(
+            <BrowserRouter>
+                <Provider store={appStore}>
+                    <Header />
+                    <RestaurantMenu />
+                    <Cart />
+                </Provider>
+            </BrowserRouter>
+        ));
+
+        const accordionHeader = screen.getByText("Chinese Economic Combo [2 Boxes] (6)");
+        fireEvent.click(accordionHeader);
+
+        expect(screen.getAllByTestId('foodItems').length).toBe(6);
+
+    });
+
+    it('should add the item to the cart', async () => {
+
+        await act(async () => render(
+            <BrowserRouter>
+                <Provider store={appStore}>
+                    <Header />
+                    <RestaurantMenu />
+                    <Cart />
+                </Provider>
+            </BrowserRouter>
+        ));
+
+        const accordionHeader = screen.getByText("Chinese Economic Combo [2 Boxes] (6)");
+        fireEvent.click(accordionHeader);
+
+        const addBtns = screen.getAllByRole('button', { name: 'Add +' });
+        fireEvent.click(addBtns[0]);
+
+        const cartItem = screen.getByText('Cart - (1 Items)')
+
+        expect(cartItem).toBeInTheDocument();
+
+        // Add 2 Items
+        fireEvent.click(addBtns[1]);
+        expect(screen.getByText('Cart - (2 Items)')).toBeInTheDocument();
+
+        // It is showing 7 because default items were 6 and then we added one and we are re-using the ItemList Component
+        const ItemsAdded = screen.getAllByTestId('foodItems');
+        expect(ItemsAdded.length).toBe(8);
+
+        // Clearing the cart
+        const clearCart = screen.getByRole('button', { name: 'Clear Cart' });
+        fireEvent.click(clearCart);
+
+        // After clearing the cart the length should be now 6
+        expect(screen.getAllByTestId('foodItems').length).toBe(6);
+
+
+
+
+    });
 
 });
