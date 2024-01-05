2ec886ed6b929c47a2e35d8e0cf02498859d8dab
junaid <jaanusd79@gmail.com>
2024-01-05 21:48:26 +0530

    Updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -60,7 +60,6 @@ const Body = () => {
                                     <RestaurantCard resList={restaurant} />
                             }
                             </Link></div>))
-
                     }
                 </div>
             </div>
